e38911905bfb3d620eea86661aee641e3a116d84
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-01-06 19:50:34 +0500

    initial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 dotenv.config({ path: "./config.env" });
 mongoose.connect(process.env.DB);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "", () => {
   console.log("Server is running in " + PORT);
 });
