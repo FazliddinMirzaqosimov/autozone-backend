8f9184cdfe9f9610e716d288390ff18235622679
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-01-11 21:41:37 +0500

    bug fixed

--- model/productModel.js
+++ model/productModel.js
@@ -1,50 +1,49 @@
 const mongoose = require("mongoose");
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: [true, "You must give name"],
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "You must give name"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Price isn't defined"],
+    },
+    category: {
+      type: String,
+      required: [true, "Category isn't defined"],
+    },
+    car: {
+      type: String,
+      required: [true, "Car isn't defined"],
+    },
+    image: {
+      type: String,
+      required: [true, "Car isn't defined"],
+    },
+    imageName: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    capacity: {
+      type: Number,
+    },
+    unity: {
+      type: String,
+    },
   },
-  price: {
-    type: Number,
-    required: [true, "Price isn't defined"],
-  },
-  category: {
-    type: String,
-    required: [true, "Category isn't defined"],
-  },
-  car: {
-    type: String,
-    required: [true, "Car isn't defined"],
-  },
-  image: {
-    type: String,
-    required: [true, "Car isn't defined"],
-  },
-  imageName: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-  rating: {
-    type: Number,
-    min: 0,
-    max: 5,
-  },
-  capacity: {
-    type: Number,
-  },
-  unity: {
-    type: String,
-  },
-  created: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Product", productSchema);
